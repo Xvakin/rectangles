f1402beb53a687814c88d1b33dee6ee849a2fd47
ivan zhevakin <ivan.zhevakin@omnigon.com>
2015-10-26 20:01:15 +0300

    Rectangle class updated

--- js/Rectangle.js
+++ js/Rectangle.js
@@ -4,22 +4,25 @@
     global.rectanglesApp = global.rectanglesApp || {};
 
     global.rectanglesApp.Rectangle = global.rectanglesApp.Class({
-        init: function (width, height, color) {
+        init: function (container, width, height, color) {
+            this.container = container;
             this.width = width;
             this.height = height;
             this.color = color;
         },
-        render: function (container) {
+        render: function () {
             var el = global.document.createElement('div');
+            this.container.innerHTML = '';
             el.style.width = this.width + 'px';
             el.style.height = this.height + 'px';
             el.style.backgroundColor = this.color;
-            container.appendChild(el)
+            this.container.appendChild(el);
         },
         update: function (width, height, color) {
             this.width = width;
             this.height = height;
             this.color = color;
+            this.render();
         }
     });
 
--- js/RectangleList.js
+++ js/RectangleList.js
@@ -6,46 +6,50 @@
     global.rectanglesApp.RectangleList = global.rectanglesApp.Class({
         init: function (container) {
             this.container = container;
-            this.rects = [
-                new global.rectanglesApp.Rectangle(100, 100, 'pink'),
-                new global.rectanglesApp.Rectangle(100, 50, 'blue')
+            this.rects = [];
+            this.rectsData = [
+                {width: 100, height: 50, color: 'pink'},
+                {width: 100, height: 50, color: 'blue'}
             ];
-            this.render();
-            this.bindEvents();
-
             this.editForm = new rectanglesApp.RectangleEditForm();
+
+            this.bindEvents();
+            this.render();
         },
         render: function () {
-            var el, link;
             this.container.innerHTML = '';
-            for (var i = 0, ii = this.rects.length; i < ii; i++) {
-                el = global.document.createElement('div');
-                link = global.document.createElement('a');
-                link.setAttribute('href', '#');
-                link.textContent = 'Edit';
-                link.addEventListener('click', (function (i, el) {
-                    return function () {
-                        this.editRect(this.rects[i], el);
-                    }
-                })(i, el).bind(this));
-                el.appendChild(link);
-                this.container.appendChild(el);
-                this.rects[i].render(el);
+            for (var i = 0, ii = this.rectsData.length; i < ii; i++) {
+                this.addRect(this.rectsData[i].width, this.rectsData[i].height, this.rectsData[i].color);
             }
         },
         bindEvents: function () {
-            document.getElementById('app-add-rect').addEventListener('click', this.addRect.bind(this));
+            document.getElementById('app-add-rect').addEventListener('click', this.addNewRect.bind(this));
         },
         addRect: function (width, height, color) {
-            var newRect = new global.rectanglesApp.Rectangle(100, 100, 'gray');
-            this.rects.push(newRect);
-            this.render();
+            var el, link, rectEl, rect;
+            el = global.document.createElement('div');
+            rectEl = global.document.createElement('div');
+            rect = new global.rectanglesApp.Rectangle(rectEl, width, height, color);
+            this.rects.push(rect);
+            link = global.document.createElement('a');
+            link.setAttribute('href', '#');
+            link.textContent = 'Edit';
+            link.addEventListener('click', (function (el) {
+                return function () {
+                    this.editRect(rect, el);
+                }
+            })(el).bind(this));
+            el.appendChild(link);
+            el.appendChild(rectEl);
+            this.container.appendChild(el);
+            rect.render(el);
+        },
+        addNewRect: function () {
+            this.addRect(100, 100, 'gray');
         },
         editRect: function (rect, el) {
             this.editForm.showForm(el);
             this.editForm.setCurrentRect(rect);
-            rect.update(200, 50, 'green');
-            //this.render();
         }
     });
 
